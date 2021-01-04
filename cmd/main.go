2d99e3e2f0d4daaf5aa84052033d37dec528da23
Mark Poling <mark.poling@gmail.com>
2021-01-04 11:33:32 -0500

    basics

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// Person is a Person struct
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	LastName  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+	Age       int32              `json:"age,omitempty" bson:"age,omitempty"`
+}
 
 func main() {
 
-	p := Person{
-		ID:        [12]byte{},
-		LastName:  "Inu",
-		FirstName: "Haku",
-		Age:       3,
-	}
-	p.Age = 5
-
 	fmt.Println("So it begins.")
-	fmt.Println(p)
 	r := mux.NewRouter()
 	routes(r)
 
@@ -36,6 +41,7 @@ func main() {
 func routes(r *mux.Router) {
 	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/person", personHandler)
+	r.HandleFunc("/persons", personsHandler)
 
 }
 
@@ -46,4 +52,33 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func personHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello Person!")
 
+}
+
+func personsHandler(w http.ResponseWriter, r *http.Request) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://mongouser:hpkns372@cluster0.dfch4.mongodb.net/test?retryWrites=true&w=majority",
+	))
+	if err != nil {
+		log.Fatal(err)
+	}
+	goofDB := client.Database("test")
+	goofCollection := goofDB.Collection("persons")
+
+	cursor, err := goofCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var persons []Person
+	if err = cursor.All(ctx, &persons); err != nil {
+		log.Fatal(err)
+	}
+	for _, person := range persons {
+		fmt.Fprintf(w, "%v\n", person.ID)
+		fmt.Fprintf(w, "\t%v\n", person.FirstName)
+		fmt.Fprintf(w, "\t%v\n", person.LastName)
+		fmt.Fprintf(w, "\t%v\n", person.Age)
+	}
 }
